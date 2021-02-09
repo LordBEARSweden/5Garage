ce63a3a55341ec0a8be73e48add5c5e4469b0bf9
Björn Andersson <lord_bear@hotmail.com>
2021-02-09 13:15:47 +0100

    Add project files.

---
+++ Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5Garage
+{
+    class Vehicle
+    {
+        public string RegistrationNumber { get; set; }
+        public string Color { get; set; }
+        public int NumberOfWheels { get; set; }
+        public int NumberOfSeats { get; set; }
+
+    }
+}
